efa3c687b834b8fd076692a6c4fdd04e2daa4342
mayurS <mayurs@prdxn.com>
2020-10-27 12:31:42 +0530

    add script for reset form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,12 @@
-/* Author: 
-
-*/
-
-
+var form = document.querySelector('form');
+var submit = document.querySelector('.submit');
+var reserForm = document.querySelector('.cancle');
 
 
+submit.addEventListener('click', function(){
 
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reserForm.addEventListener('click', function(){
+  form.reset();
+});

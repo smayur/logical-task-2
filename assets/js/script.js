a4509542d27b5f41899ab7fa7ecf9c289e247cb7
mayurS <mayurs@prdxn.com>
2020-10-27 12:55:28 +0530

    add variable gender

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var form = document.querySelector('form');
+var gender = document.querySelectorAll('[gender]');
 var submit = document.querySelector('.submit');
 var reserForm = document.querySelector('.cancle');
 
 
 submit.addEventListener('click', function(){
-
+  
 });
 
 reserForm.addEventListener('click', function(){

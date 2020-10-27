fad1fdff6b5239e3a321273631c646b6ff2d1e05
mayurS <mayurs@prdxn.com>
2020-10-27 13:50:44 +0530

    script for gender

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var form = document.querySelector('form');
-var gender = document.querySelectorAll('[gender]');
+var gender = document.getElementsByName('gender');
 var submit = document.querySelector('.submit');
 var reserForm = document.querySelector('.cancle');
 
 
-submit.addEventListener('click', function(){
-  console.log(gender);
+submit.addEventListener('click', function(e){
+  e.preventDefault();
+  for (var index = 0; index < gender.length; index++) {
+    if (gender[index].checked) {
+      console.log(gender[index]);   
+    }
+  }
 });
-
-// reserForm.addEventListener('click', function(){
-//   form.reset();
-// });
